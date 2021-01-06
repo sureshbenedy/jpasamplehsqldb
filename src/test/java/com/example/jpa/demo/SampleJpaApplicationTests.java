825acf2368d49dc869420bdaa3a1b263798dcfbf
lord <suresh.benedy@gmail.com>
2021-01-06 09:31:11 +0530

    minimal hsqldb jpa

---
+++ src/test/java/com/example/jpa/demo/SampleJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
